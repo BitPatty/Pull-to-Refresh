1a950c4fe947667e600bf1b8d2f4170ea265e4fe
Petre Tudor <petre.tudor@dorkfarm.com>
2019-12-27 19:38:15 +0100

    added comments and pushed version to 0.4

--- pull-to-refresh.js
+++ pull-to-refresh.js
@@ -1,28 +1,37 @@
 const tresholdMoveX = 20; // max percentage moving on X
-const tresholdMoveY = 30; // max percentage moving on Y
+const tresholdMoveY = 30; // min percentage moving on Y
 
-const touchEventElement = document.documentElement;
+// get page height and  width
 const height = window.screen.height;
 const width = window.screen.width;
 
-
+// init touch start vars
 let startClientX = null;
 let startClientY = null;
 
-touchEventElement.addEventListener('touchstart', (e) => {
+// listen for touchstart events
+document.documentElement.addEventListener('touchstart', (e) => {
+    // get location of initial touch 
     startClientX = e.touches[0].clientX;
     startClientY = e.touches[0].clientY;
 });
 
 
-touchEventElement.addEventListener('touchend', (e) => {
+// listen for touchend events
+document.documentElement.addEventListener('touchend', (e) => {
+    // get location of end touch 
     let endClientX = e.changedTouches[0].clientX;
     let endClientY = e.changedTouches[0].clientY;
     
+    // calculate gesture movement on X and Y as percentage of page size
     const percentageMoveX = (startClientX - endClientX) / width * 100;
     const percentageMoveY = (endClientY - startClientY) / width * 100;
     
-    if (window.scrollY === 0 && percentageMoveY > 0 && percentageMoveX < Math.abs(tresholdMoveX) && Math.abs(percentageMoveY) > 30) {
+    if (window.scrollY === 0 // if the window is at the top
+        && percentageMoveY > 0 // and movement on Y is downwards
+        && percentageMoveX < Math.abs(tresholdMoveX) // and movemenent on X is maximum tresholdMoveY
+        && Math.abs(percentageMoveY) >= 30 // and movement on Y is minimum percentageMoveY
+    ){
         window.location.reload(true);
     }
 });
